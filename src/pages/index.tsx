9d282cfbe0243d251c2a36f5b6bac1e76f92be5a
pmass <pmass@DESKTOP-04787T0>
2024-05-10 11:26:07 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Layout from '@/componets/Layout'
 import { withAuth } from '@/componets/withAuth/withAuth'
+import Layout from '@/componets/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Home = () => (
-  <Layout title="Home" children={undefined}>
+  <Layout title="Home">
     
   </Layout>
 );
